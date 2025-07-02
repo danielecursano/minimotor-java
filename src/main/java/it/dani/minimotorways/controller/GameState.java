5753ff187924e4a171949562d4e49a26c4a2eb28
danielecursano <dacu1405@gmail.com>
2025-07-02 23:04:28 +0200

    add: score and num of roads

--- src/main/java/it/dani/minimotorways/controller/GameState.java
+++ src/main/java/it/dani/minimotorways/controller/GameState.java
@@ -8,11 +8,15 @@ import java.util.Map;
 public class GameState {
     public Map<Integer, Building> buildings;
     public Map<Integer, Car> cars;
+    public final int score;
     public State state;
+    public final int numOfRoads;
 
-    public GameState(Map<Integer, Building> buildings, Map<Integer, Car> cars, State state) {
+    public GameState(Map<Integer, Building> buildings, Map<Integer, Car> cars, State state, int score, int numOfRoads) {
         this.buildings = buildings;
         this.cars = cars;
         this.state = state;
+        this.score = score;
+        this.numOfRoads = numOfRoads;
     }
 }
